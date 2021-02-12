18f3a3d8aa665aaa587050d0a6a2c13f80349ff3
jastakei <jas.takei@gmail.com>
2021-02-12 00:04:28 -0800

    testing custom component idk what is going on

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,11 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if (localStorage.getItem("array") === null) {
+    let response = await fetch('https://fakestoreapi.com/products');
+    let text = await response.text();
+    localStorage.setItem('array', text);
+  }
+  
+  
 });
