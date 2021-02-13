d65d40d669d0808e87864c969aa87a76e65f1e64
jastakei <jas.takei@gmail.com>
2021-02-13 01:03:00 -0800

    +script

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,19 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
-  if (localStorage.getItem("array") === null) {
-    var response = fetch('https://fakestoreapi.com/products');
-    let text = response.text();
-    localStorage.setItem('array', text);
-  }
+  //if (localStorage.getItem("array") === null) {
+    
+ fetch('https://fakestoreapi.com/products') 
+  .then(response => response.json())
+  .then(result => localStorage.setItem('array', JSON.stringify(result)))
+  //}
 
 
 });
 window.addEventListener("load", function(){
 var node = document.createElement("product-item");
+    node.setAttribute('item', "idkkkk");
+    
 //node.setAttribute()
 document.getElementById("product-list").appendChild(node);
 });
