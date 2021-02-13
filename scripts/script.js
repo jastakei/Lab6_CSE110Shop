0ce7a861a8e6136198f01a0f3852adba2e5a3ce9
jastakei <jas.takei@gmail.com>
2021-02-12 19:45:26 -0800

    changed fetch in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,15 @@
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   if (localStorage.getItem("array") === null) {
-    let response = await fetch('https://fakestoreapi.com/products');
-    let text = await response.text();
+    var response = fetch('https://fakestoreapi.com/products');
+    let text = response.text();
     localStorage.setItem('array', text);
   }
-  
-  
+
+
+});
+window.addEventListener("load", function(){
+var node = document.createElement("product-item");
+//node.setAttribute()
+document.getElementById("product-list").appendChild(node);
 });
